594592922eefff7f19b95ad7bb965b114a2ca64a
Leigh MacDonald <leigh.macdonald@gmail.com>
2026-02-11 12:42:46 -0700

    Add dev shell

---
+++ shell.nix
@@ -0,0 +1,25 @@
+#{ lib, ... }:
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
+
+  pkgs = import nixpkgs {
+    config = {};
+    overlays = [];
+  };
+in
+  pkgs.mkShell {
+    #  LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.openssl ];
+    hardeningDisable = ["fortify"];
+    buildInputs = with pkgs; [
+      pkg-config
+      openssl
+      cmake
+      zlib
+      libgit2
+      rust-analyzer
+      cargo-audit
+      goreleaser
+      zig # required by goreleaser
+      just
+    ];
+  }
